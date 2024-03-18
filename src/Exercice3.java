8143dffbe6d7c480086cabd9469280a0ee365463
Zaher Madi <zahermadi@yahoo.fr>
2024-03-18 12:51:48 +0100

    Exo3!!

--- src/Exercice3.java
+++ src/Exercice3.java
@@ -6,10 +6,47 @@ public class Exercice3 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.printf("\n T'as eu");
+        System.out.printf("\n T'as combien de moyenne en math ? \n ---> ");
 
         //val reel :
         float noteMath = sc.nextFloat();
+
+        System.out.printf("\n T'as combien de moyenne en Français ? \n ---> ");
+
+        //val reel :
+        float noteFR = sc.nextFloat();
+
+
+        System.out.printf("\n T'as combien de moyenne en Anglais ? \n ---> ");
+
+        //val reel :
+        float noteEN = sc.nextFloat();
+
+        System.out.printf("Allons calculer ta moyenne ...\n ");
+        Thread.sleep(500);
+        System.out.printf("...");
+        Thread.sleep(500);
+        System.out.printf("...");
+        Thread.sleep(500);
+        System.out.printf("...");
+
+        float somme = (noteEN + noteFR + noteMath)/3;
+
+        System.out.printf("\n Moyenne générale :...\n " + somme);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
